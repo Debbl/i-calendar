b2318e4cde766532200ed00ba2f0b779c9855a3d
Debbl <me@aiwan.run>
2023-02-07 22:07:19 +0800

    refactor: remove antd and normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,60 @@
-import { Badge, Calendar } from "antd";
-import type { BadgeProps } from "antd";
 import dayjs from "dayjs";
-import type { Dayjs } from "dayjs";
 import { useEffect, useState } from "react";
 import ICalParser from "ical-js-parser";
 
+interface ICSData {
+  calendar: {};
+  events: Array<{
+    location: string;
+    description: string;
+    url: string;
+    dtstamp: {
+      timezone: string;
+      value: string;
+    };
+    dtstart: {
+      timezone: string;
+      value: string;
+    };
+  }>;
+}
+type WeekData = Array<{
+  content: Array<{
+    name: string;
+    liveURL: string;
+  }>;
+}>;
 const url = "https://nebula-beat.github.io/xld.ics";
 
-const monthCellRender = (value: Dayjs) => {
-  const getMonthData = (value: Dayjs) => {
-    if (value.month() === 8) {
-      return 1394;
-    }
-  };
-
-  const num = getMonthData(value);
-  return num ? (
-    <div className="notes-month">
-      <section>{num}</section>
-      <span>Backlog number</span>
-    </div>
-  ) : null;
-};
-
 const App: React.FC = () => {
-  const [iscData, setIscData] = useState<{
-    calendar: {};
-    events: Array<{
-      location: string;
-      description: string;
-      dtstamp: {
-        timezone: string;
-        value: string;
-      };
-    }>;
-  }>();
+  const [iscData, setIscData] = useState<ICSData>();
+
   useEffect(() => {
     fetch(url)
       .then((res) => res.text())
       .then((data) => {
-        setIscData(ICalParser.toJSON(data) as any);
+        const icsData = ICalParser.toJSON(data) as any as ICSData;
+        setIscData(icsData);
       });
   }, []);
-  const dataCellRender = (value: Dayjs) => {
-    const getListData = (value: Dayjs) => {
-      const listData: Array<{
-        type: string;
-        content: string;
-      }> = [];
-      iscData?.events.forEach((e) => {
-        if (dayjs(e.dtstamp.value).dayOfYear() === value.dayOfYear()) {
-          console.log(e);
-          listData.push({ type: "success", content: e.location.trim() });
-        }
-      });
 
-      return listData || [];
-    };
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge
-              status={item.type as BadgeProps["status"]}
-              text={item.content}
-            />
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  return (
-    <div>
-      <div>
-        <Calendar
-          dateCellRender={dataCellRender}
-          monthCellRender={monthCellRender}
-        />
-      </div>
-    </div>
-  );
+  const weekData: WeekData = Array.from({ length: 7 }, () => ({
+    content: [],
+  }));
+
+  iscData?.events.forEach((e) => {
+    const dtsart = dayjs(e.dtstart.value);
+    const dayOffset = dtsart.day() - 1;
+    const index = dayOffset === -1 ? 6 : dayOffset;
+
+    weekData[index].content.push({
+      name: e.location.split(" ")[0],
+      liveURL: e.description.split("\\n")[0],
+    });
+  });
+  console.log(weekData);
+
+  return <div></div>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import dayjs from "dayjs";
 import dayOfYear from "dayjs/plugin/dayOfYear";
+import weekday from "dayjs/plugin/weekday";
 import App from "./App";
 import "antd/dist/reset.css";
 import "normalize.css";
 import "./index.css";
 
 dayjs.extend(dayOfYear);
+dayjs.extend(weekday);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
