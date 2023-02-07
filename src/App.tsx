d9553d348ac2aa9c98523cef25c55ce73c3d82f2
Debbl <me@aiwan.run>
2023-02-07 20:02:50 +0800

    feat: basic work

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Badge, Calendar } from "antd";
+import type { BadgeProps } from "antd";
+import dayjs from "dayjs";
+import type { Dayjs } from "dayjs";
+import { useEffect, useState } from "react";
+import ICalParser from "ical-js-parser";
 
-function App() {
-  const [count, setCount] = useState(0);
+const url = "https://nebula-beat.github.io/xld.ics";
 
+const monthCellRender = (value: Dayjs) => {
+  const getMonthData = (value: Dayjs) => {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  };
+
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+};
+
+const App: React.FC = () => {
+  const [iscData, setIscData] = useState<{
+    calendar: {};
+    events: Array<{
+      location: string;
+      description: string;
+      dtstamp: {
+        timezone: string;
+        value: string;
+      };
+    }>;
+  }>();
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.text())
+      .then((data) => {
+        setIscData(ICalParser.toJSON(data) as any);
+      });
+  }, []);
+  const dataCellRender = (value: Dayjs) => {
+    const getListData = (value: Dayjs) => {
+      const listData: Array<{
+        type: string;
+        content: string;
+      }> = [];
+      iscData?.events.forEach((e) => {
+        if (dayjs(e.dtstamp.value).dayOfYear() === value.dayOfYear()) {
+          console.log(e);
+          listData.push({ type: "success", content: e.location.trim() });
+        }
+      });
+
+      return listData || [];
+    };
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge
+              status={item.type as BadgeProps["status"]}
+              text={item.content}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  };
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Calendar
+          dateCellRender={dataCellRender}
+          monthCellRender={monthCellRender}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import dayjs from "dayjs";
+import dayOfYear from "dayjs/plugin/dayOfYear";
 import App from "./App";
-import "./index.css";
+import "antd/dist/reset.css";
+import "normalize.css";
+
+dayjs.extend(dayOfYear);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
