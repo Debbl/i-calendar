b133a32649a525b20b053dc9fd079a1fe778a085
Debbl <me@aiwan.run>
2023-02-12 13:57:15 +0800

    fix: small screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,18 @@ const App: React.FC = () => {
   const { weekData, calenderInfo } = useWeekData(url);
 
   return (
-    <div className="flex flex-col items-center gap-y-1">
-      <span className="text-gray-500">{url}</span>
-      <span>{calenderInfo?.xWrCalname}</span>
+    <div>
+      <div className="text-center text-gray-500">{url}</div>
+      <div className="text-center">{calenderInfo?.xWrCalname}</div>
+
       <TimeClick className="fixed right-6 top-6 text-lg font-medium" />
-      <div className="mt-2 flex justify-center gap-x-1">
-        {weekData.map((d, i) => (
-          <WeekList key={i} d={d} />
-        ))}
+
+      <div className="text-center">
+        <div className="mt-2 inline-flex justify-center gap-x-1">
+          {weekData.map((d, i) => (
+            <WeekList key={i} d={d} />
+          ))}
+        </div>
       </div>
     </div>
   );
